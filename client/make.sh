798ea7d80e66183c4bb33d52b2d82a635947ab51
Matt Corallo <git@bluematt.me>
2014-09-04 03:38:51 -0700

    Add C++ client

---
+++ client/make.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+i686-w64-mingw32-g++ -DNDEBUG -std=c++11 -Wall -O2 -DWIN32 -mno-ms-bitfields -static -static-libgcc -I. ./client.cpp ./crypto/sha2.cpp -lwsock32 -lmingwthrd -o relaynetworkclient.exe
+clang++ -DNDEBUG -std=c++11 -Wall -O2 -pthread -I. -I/usr/include ./client.cpp ./crypto/sha2.cpp -o relaynetworkclient
+g++ -DNDEBUG -std=c++11 -Wall -O2 -pthread -I. -I/usr/include ./client.cpp ./crypto/sha2.cpp -o relaynetworkclient
