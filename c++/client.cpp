62fe2354b9feede8e0bd7bf875e4e5512a2ed60c
rushing <gitsam@rushing.nightmare.com>
2014-10-29 20:47:32 +0000

    need stdio, sys/socket, netinet/in.h.

--- c++/client.cpp
+++ c++/client.cpp
@@ -6,11 +6,14 @@
 #include <assert.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #ifdef WIN32
 	#include <winsock2.h>
 	#include <ws2tcpip.h>
 #else // WIN32
+	#include <sys/socket.h>
+	#include <netinet/in.h>
 	#include <netinet/tcp.h>
 	#include <netdb.h>
 	#include <fcntl.h>
