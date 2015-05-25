33846a3009267e2e280eee1a972917d298d35f68
Matt Corallo <git@bluematt.me>
2015-05-24 17:14:38 -0700

    Move compressor code to cpp file

--- c++/relayprocess.h
+++ c++/relayprocess.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <tuple>
 #include <thread>
+#include <mutex>
 
 #include "mruset.h"
 #include "flaggedarrayset.h"
@@ -25,15 +26,14 @@ private: \
 class RelayNodeCompressor {
 	RELAY_DECLARE_CLASS_VARS
 
-protected:
-	FlaggedArraySet send_tx_cache;
 private:
-	FlaggedArraySet recv_tx_cache;
+	FlaggedArraySet send_tx_cache, recv_tx_cache;
 	mruset<std::vector<unsigned char> > blocksAlreadySeen;
 	std::mutex mutex;
 
 public:
 	RelayNodeCompressor() : RELAY_DECLARE_CONSTRUCTOR_EXTENDS, send_tx_cache(1525), recv_tx_cache(1525), blocksAlreadySeen(10) {}
+	void reset();
 
 	inline std::shared_ptr<std::vector<unsigned char> > tx_to_msg(const std::shared_ptr<std::vector<unsigned char> >& tx) {
 		auto msg = std::make_shared<std::vector<unsigned char> > (sizeof(struct relay_msg_header));
@@ -44,180 +44,20 @@ public:
 		msg->insert(msg->end(), tx->begin(), tx->end());
 		return msg;
 	}
+	std::shared_ptr<std::vector<unsigned char> > get_relay_transaction(const std::shared_ptr<std::vector<unsigned char> >& tx);
 
-	std::shared_ptr<std::vector<unsigned char> > get_relay_transaction(const std::shared_ptr<std::vector<unsigned char> >& tx) {
-		std::lock_guard<std::mutex> lock(mutex);
+	bool maybe_recv_tx_of_size(uint32_t tx_size, bool debug_print);
+	void recv_tx(std::shared_ptr<std::vector<unsigned char > > tx);
 
-		if (send_tx_cache.contains(tx) ||
-				(tx->size() > MAX_RELAY_TRANSACTION_BYTES &&
-					(send_tx_cache.flagCount() >= MAX_EXTRA_OVERSIZE_TRANSACTIONS || tx->size() > MAX_RELAY_OVERSIZE_TRANSACTION_BYTES)))
-			return std::shared_ptr<std::vector<unsigned char> >();
-		send_tx_cache.add(tx, tx->size() > MAX_RELAY_TRANSACTION_BYTES);
-		return tx_to_msg(tx);
-	}
+	void for_each_sent_tx(const std::function<void (std::shared_ptr<std::vector<unsigned char> >)> callback);
 
-	void reset() {
-		recv_tx_cache.clear();
-		send_tx_cache.clear();
-	}
-
-	bool maybe_recv_tx_of_size(uint32_t tx_size, bool debug_print) {
-		if (tx_size > MAX_RELAY_TRANSACTION_BYTES && (recv_tx_cache.flagCount() >= MAX_EXTRA_OVERSIZE_TRANSACTIONS || tx_size > MAX_RELAY_OVERSIZE_TRANSACTION_BYTES)) {
-			if (debug_print)
-				printf("Freely relayed tx of size %u, with %lu oversize txn already present\n", tx_size, recv_tx_cache.flagCount());
-			return false;
-		}
-		return true;
-	}
-
-	void recv_tx(std::shared_ptr<std::vector<unsigned char > > tx) {
-		uint32_t tx_size = tx.get()->size();
-		assert(maybe_recv_tx_of_size(tx_size, true));
-		recv_tx_cache.add(tx, tx_size > MAX_RELAY_TRANSACTION_BYTES);
-	}
-
-	void for_each_sent_tx(const std::function<void (std::shared_ptr<std::vector<unsigned char> >)> callback) {
-		std::lock_guard<std::mutex> lock(mutex);
-		send_tx_cache.for_all_txn(callback);
-	}
-
-	std::shared_ptr<std::vector<unsigned char> > maybe_compress_block(const std::vector<unsigned char>& hash, const std::vector<unsigned char>& block) {
-		std::lock_guard<std::mutex> lock(mutex);
-
-		if (!blocksAlreadySeen.insert(hash).second)
-			return std::shared_ptr<std::vector<unsigned char> >();
-
-		auto compressed_block = compressRelayBlock(block);
-		if (!compressed_block->size()) {
-			printf("Failed to process block from bitcoind\n");
-			return std::shared_ptr<std::vector<unsigned char> >();
-		}
-
-		return compressed_block;
-	}
-
-	std::tuple<uint32_t, std::shared_ptr<std::vector<unsigned char> >, const char*, std::shared_ptr<std::vector<unsigned char> > > decompress_relay_block(int sock, uint32_t message_size) {
-		std::lock_guard<std::mutex> lock(mutex);
-
-		auto res = decompressRelayBlock(sock, message_size);
-
-		auto fullhashptr = std::make_shared<std::vector<unsigned char> > (32);
-		getblockhash(*fullhashptr.get(), *std::get<1>(res), sizeof(struct bitcoin_msg_header));
-		blocksAlreadySeen.insert(*fullhashptr.get());
-
-		return std::make_tuple(std::get<0>(res), std::get<1>(res), std::get<2>(res), fullhashptr);
-	}
+	std::shared_ptr<std::vector<unsigned char> > maybe_compress_block(const std::vector<unsigned char>& hash, const std::vector<unsigned char>& block);
+	std::tuple<uint32_t, std::shared_ptr<std::vector<unsigned char> >, const char*, std::shared_ptr<std::vector<unsigned char> > > decompress_relay_block(int sock, uint32_t message_size);
 
 private:
-	std::shared_ptr<std::vector<unsigned char> > compressRelayBlock(const std::vector<unsigned char>& block) {
-		auto compressed_block = std::make_shared<std::vector<unsigned char> >();
-		compressed_block->reserve(1100000);
-		struct relay_msg_header header;
-
-		try {
-			std::vector<unsigned char>::const_iterator readit = block.begin();
-			move_forward(readit, sizeof(struct bitcoin_msg_header), block.end());
-			move_forward(readit, 80, block.end());
-			uint32_t txcount = read_varint(readit, block.end());
-
-			header.magic = RELAY_MAGIC_BYTES;
-			header.type = BLOCK_TYPE;
-			header.length = htonl(txcount);
-			compressed_block->insert(compressed_block->end(), (unsigned char*)&header, ((unsigned char*)&header) + sizeof(header));
-			compressed_block->insert(compressed_block->end(), block.begin() + sizeof(struct bitcoin_msg_header), block.begin() + 80 + sizeof(struct bitcoin_msg_header));
-
-			for (uint32_t i = 0; i < txcount; i++) {
-				std::vector<unsigned char>::const_iterator txstart = readit;
-
-				move_forward(readit, 4, block.end());
-
-				uint32_t txins = read_varint(readit, block.end());
-				for (uint32_t j = 0; j < txins; j++) {
-					move_forward(readit, 36, block.end());
-					uint32_t scriptlen = read_varint(readit, block.end());
-					move_forward(readit, scriptlen + 4, block.end());
-				}
-
-				uint32_t txouts = read_varint(readit, block.end());
-				for (uint32_t j = 0; j < txouts; j++) {
-					move_forward(readit, 8, block.end());
-					uint32_t scriptlen = read_varint(readit, block.end());
-					move_forward(readit, scriptlen, block.end());
-				}
-
-				move_forward(readit, 4, block.end());
-
-				auto lookupVector = std::make_shared<std::vector<unsigned char> >(txstart, readit);
-				int index = send_tx_cache.remove(lookupVector);
-				if (index < 0) {
-					compressed_block->push_back(0xff);
-					compressed_block->push_back(0xff);
-
-					uint32_t txlen = readit - txstart;
-					compressed_block->push_back((txlen >> 16) & 0xff);
-					compressed_block->push_back((txlen >>  8) & 0xff);
-					compressed_block->push_back((txlen      ) & 0xff);
-
-					compressed_block->insert(compressed_block->end(), txstart, readit);
-				} else {
-					compressed_block->push_back((index >> 8) & 0xff);
-					compressed_block->push_back((index     ) & 0xff);
-				}
-			}
-		} catch(read_exception) {
-			return std::make_shared<std::vector<unsigned char> >();
-		}
-		return compressed_block;
-	}
-
-	std::tuple<uint32_t, std::shared_ptr<std::vector<unsigned char> >, const char*> decompressRelayBlock(int sock, uint32_t message_size) {
-		if (message_size > 100000)
-			return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "got a BLOCK message with far too many transactions");
-
-		uint32_t wire_bytes = 4*3;
-
-		auto block = std::make_shared<std::vector<unsigned char> > (sizeof(bitcoin_msg_header) + 80);
-		block->reserve(1000000);
-
-		if (read_all(sock, (char*)&(*block)[sizeof(bitcoin_msg_header)], 80) != 80)
-			return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "failed to read block header");
-
-		auto vartxcount = varint(message_size);
-		block->insert(block->end(), vartxcount.begin(), vartxcount.end());
-
-		for (uint32_t i = 0; i < message_size; i++) {
-			uint16_t index;
-			if (read_all(sock, (char*)&index, 2) != 2)
-				return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "failed to read tx index");
-			index = ntohs(index);
-			wire_bytes += 2;
-
-			if (index == 0xffff) {
-				union intbyte {
-					uint32_t i;
-					char c[4];
-				} tx_size {0};
-
-				if (read_all(sock, tx_size.c + 1, 3) != 3)
-					return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "failed to read tx length");
-				tx_size.i = ntohl(tx_size.i);
-
-				if (tx_size.i > 1000000)
-					return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "got unreasonably large tx ");
-
-				block->insert(block->end(), tx_size.i, 0);
-				if (read_all(sock, (char*)&(*block)[block->size() - tx_size.i], tx_size.i) != int64_t(tx_size.i))
-					return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "failed to read transaction data");
-				wire_bytes += 3 + tx_size.i;
-			} else {
-				std::shared_ptr<std::vector<unsigned char> > transaction_data = recv_tx_cache.remove(index);
-				if (!transaction_data->size())
-					return std::make_tuple(0, std::shared_ptr<std::vector<unsigned char> >(NULL), "failed to find referenced transaction");
-				block->insert(block->end(), transaction_data->begin(), transaction_data->end());
-			}
-		}
-		return std::make_tuple(wire_bytes, block, (const char*) NULL);
-	}
+	bool check_recv_tx(uint32_t tx_size);
+	std::shared_ptr<std::vector<unsigned char> > compressRelayBlock(const std::vector<unsigned char>& block);
+	std::tuple<uint32_t, std::shared_ptr<std::vector<unsigned char> >, const char*> decompressRelayBlock(int sock, uint32_t message_size);
 };
 
 #endif
