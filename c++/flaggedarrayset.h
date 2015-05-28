a4a34f3efae2cbb06b67f28444feb19e2b0394f7
Matt Corallo <git@bluematt.me>
2015-05-28 11:24:38 -0700

    SLOWER :( Use unordered map

--- c++/flaggedarrayset.h
+++ c++/flaggedarrayset.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <thread>
 #include <map>
+#include <unordered_map>
 
 /******************************
  **** FlaggedArraySet util ****
@@ -13,28 +14,38 @@ struct ElemAndFlag {
 	std::shared_ptr<std::vector<unsigned char> > elem;
 	ElemAndFlag(const std::shared_ptr<std::vector<unsigned char>>& elemIn, bool flagIn) : flag(flagIn), elem(elemIn) {}
 	ElemAndFlag() {}
-	bool operator <(const ElemAndFlag& o) const { return *elem < *o.elem; }
+	bool operator == (const ElemAndFlag& o) const { return o.elem == elem; }
 };
-
+namespace std {
+	template <> struct hash<ElemAndFlag> {
+		size_t operator()(const ElemAndFlag& e) const {
+			//Stolen from boost::hash_range
+			size_t res = 0;
+			for (const auto& v : *e.elem)
+				res ^= v + 0x9e3779b9 + (res << 6) + (res >> 2);
+			return res;
+		}
+	};
+}
 
 
 class FlaggedArraySet {
 private:
 	unsigned int maxSize, flag_count;
 	uint64_t total, offset;
-	std::map<ElemAndFlag, uint64_t> backingMap;
-	std::map<uint64_t, std::map<ElemAndFlag, uint64_t>::iterator> backingReverseMap;
+	std::unordered_map<ElemAndFlag, uint64_t> backingMap;
+	std::map<uint64_t, std::unordered_map<ElemAndFlag, uint64_t>::iterator> backingReverseMap;
 
 public:
 	void clear();
-	FlaggedArraySet(unsigned int maxSizeIn) : maxSize(maxSizeIn) { clear(); }
+	FlaggedArraySet(unsigned int maxSizeIn) : maxSize(maxSizeIn), backingMap(maxSize) { clear(); }
 
 	size_t size() { return backingMap.size(); }
 	size_t flagCount() { return flag_count; }
 	bool contains(const std::shared_ptr<std::vector<unsigned char> >& e);
 
 private:
-	void remove(std::map<uint64_t, std::map<ElemAndFlag, uint64_t>::iterator>::iterator rm);
+	void remove(std::map<uint64_t, std::unordered_map<ElemAndFlag, uint64_t>::iterator>::iterator rm);
 
 public:
 	void add(const std::shared_ptr<std::vector<unsigned char> >& e, bool flag);
