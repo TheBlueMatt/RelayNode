47f11e496d624b105b35012971a8cded8920e522
Matt Corallo <git@bluematt.me>
2015-07-16 12:27:33 -0700

    Switch to system_clock because steady_clock spins

--- c++/connection.cpp
+++ c++/connection.cpp
@@ -310,17 +310,17 @@ void Connection::do_setup_and_read(Connection* me) {
 
 ssize_t Connection::read_all(char *buf, size_t nbyte, millis_lu_type max_sleep) {
 	size_t total = 0;
-	std::chrono::steady_clock::time_point stop_time;
+	std::chrono::system_clock::time_point stop_time;
 	if (max_sleep == millis_lu_type::max())
-		stop_time = std::chrono::steady_clock::time_point::max();
+		stop_time = std::chrono::system_clock::time_point::max();
 	else
-		stop_time = std::chrono::steady_clock::now() + max_sleep;
+		stop_time = std::chrono::system_clock::now() + max_sleep;
 	while (total < nbyte) {
 		std::unique_lock<std::mutex> lock(read_mutex);
-		while (!inbound_queue.size() || std::chrono::steady_clock::now() < stop_time)
+		while (!inbound_queue.size() && std::chrono::system_clock::now() < stop_time)
 			read_cv.wait_until(lock, stop_time);
 
-		if (std::chrono::steady_clock::now() >= stop_time)
+		if (std::chrono::system_clock::now() >= stop_time)
 			return total;
 
 		if (!inbound_queue.front())
